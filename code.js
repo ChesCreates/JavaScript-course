d0104e25c9de34229d33b83cbee13631c15676d0
Chesney <chesney.owino@gmail.com>
2025-06-04 12:30:31 +0300

    New file

--- code.js
+++ code.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+/* -, +, *, /, % */
+/* incrementing
+  num = num + 1; or
+  num += 1; or
+  num++; or
+  ++num;
+*/
+
+/* assignmentOperator -> '=' */
+/* comparisonOperators -> '>', '>=', '<', '<=' 
+  using these results in a boolean value - true or false
+*/
+
+let num1 = 14;
+let num2 = 10;
+
+const isNum1Greater = num1 > num2;
+
+console.log( isNum1Greater);
